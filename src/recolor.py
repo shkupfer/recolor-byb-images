af98e465f1165ef9191c90f098375984f722e0dd
shkupfer <shkupfer@ncsu.edu>
2023-02-01 21:54:48 -0500

    Nice logging

--- src/recolor.py
+++ src/recolor.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import logging
 import os
 from typing import List
 
@@ -7,6 +8,13 @@ from PIL import Image
 import numpy as np
 
 from colors import Color, Uniform, load_team_colors
+
+
+logging.basicConfig(format='%(message)s')
+LOGGER = logging.getLogger("recolor")
+LOGGER.setLevel(logging.INFO)
+
+BASE_COLOR_NAME = "Base"
 
 
 def replace_color(base_data: np.ndarray, old_color: Color, new_color: Color) -> np.ndarray:
@@ -40,14 +48,12 @@ if __name__ == "__main__":
         "--teams-file",
         "-tf",
         default="sample_data/players_teams.json",
-        required=True,
         help="A .json file containing a mapping of player names to color names and base images",
     )
     parser.add_argument(
         "--colors-file",
         "-cf",
         default="sample_data/team_colors.json",
-        required=True,
         help="A .json file containing the definition of each color referenced in the teams-file .json",
     )
     parser.add_argument(
@@ -58,26 +64,35 @@ if __name__ == "__main__":
         "These images should be colored with the base color scheme",
     )
     parser.add_argument("--output-images-dir", "-o", default="sample_data/outputs/")
-    which_players_arg_grp = parser.add_mutually_exclusive_group(required=True)
-    which_players_arg_grp.add_argument("--all", "-a", action="store_true", help="Recolor all players' images")
-    which_players_arg_grp.add_argument("--names", "-n", nargs="*", help="Recolor only the images of these players")
+    parser.add_argument(
+        "--names", "-n", nargs="*", help="Recolor only the images of these players." +
+        "If not specified, recolor all players' images",
+    )
+    parser.add_argument("--verbose", "-v", action="store_true", help="Use this to log more info")
 
     args = parser.parse_args()
+
+    if args.verbose:
+        LOGGER.setLevel(logging.DEBUG)
 
     with open(args.teams_file, "r") as teams_infile:
         players_teams_colors = json.load(teams_infile)
 
-    if args.all:
-        recolor_player_names = players_teams_colors.keys()
-    else:
+    if args.names:
         recolor_player_names = args.names
+    else:
+        # Recolor all players by default
+        recolor_player_names = players_teams_colors.keys()
+    LOGGER.info(f"Recoloring {len(recolor_player_names)} player images")
 
+    # Load team colors
     teams_colors = load_team_colors(args.colors_file)
     base_colors = teams_colors["Base"]
 
     os.makedirs(args.output_images_dir, exist_ok=True)
 
     for player_name in recolor_player_names:
+        LOGGER.debug(f"Recoloring player image: {player_name}")
         player_img_colors = players_teams_colors[player_name]
         # Get this player's primary and secondary uniform color schemes from the teams colors mapping
         primary_colors = teams_colors[player_img_colors["primary"]]
@@ -88,6 +103,7 @@ if __name__ == "__main__":
         base_image = Image.open(base_image_filename).convert("RGBA")
         base_image_data = np.array(base_image)
 
+        LOGGER.debug(f"Replacing base colors {base_colors.name} with primary colors: {primary_colors.name} and secondary colors: {secondary_colors.name}")
         # First replace the base colors with the player's primary color scheme
         recolored_with_primary = recolor_player_image(
             base_image_data, base_colors.uniform, primary_colors.uniform, garments=["hat", "jersey"]
@@ -100,4 +116,5 @@ if __name__ == "__main__":
         # Turn the image array into image and save
         final_recolored_image = Image.fromarray(recolored_with_secondary)
         output_filename = os.path.join(args.output_images_dir, player_name + ".png")
+        LOGGER.debug(f"Saving recolored {player_name} image to file {output_filename}")
         final_recolored_image.save(output_filename)
